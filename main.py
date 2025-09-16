f82215b817a226639a4b839433f2ff947f9441c5
Goh <aloysiusgoh1@kpmg.com.sg>
2025-09-16 11:24:33 +0800

    file_name

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 import base64
 import io
 import zipfile
+from datetime import datetime
 
 app = FastAPI()
 
@@ -17,11 +18,13 @@ async def convert_base64_to_xlsx(request: Request):
     try:
         # Decode the base64 string
         xlsx_bytes = base64.b64decode(base64_str)
+        now = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_name = "IFRS_File_Comparison_Report_" + now + ".xlsx"
 
         # Create an in-memory ZIP file
         zip_stream = io.BytesIO()
         with zipfile.ZipFile(zip_stream, mode="w", compression=zipfile.ZIP_DEFLATED) as zip_file:
-            zip_file.writestr("converted.xlsx", xlsx_bytes)
+            zip_file.writestr(file_name, xlsx_bytes)
 
         # Reset stream position to the beginning
         zip_stream.seek(0)
